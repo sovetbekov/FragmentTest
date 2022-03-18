27b7cf74e031d70fb7f8f0e27a4cd0a7a8cf0e6a
Talgat Sovetbekov <talat@jarvis.com>
2022-03-18 13:25:34 +0600

    initial commit

---
+++ app/src/main/java/com/example/practice2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.practice2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.graphics.Typeface
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
